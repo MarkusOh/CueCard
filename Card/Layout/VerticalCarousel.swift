463a1ea059648f642e7c8ebbd7862b40dd4f79ab
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2024-04-15 10:15:10 +0900

    Revert "subview size caching behaviour added"
    This reverts commit d07498b3780f8874bb95073f7715ffb409639dd2.

--- Card/Layout/VerticalCarousel.swift
+++ Card/Layout/VerticalCarousel.swift
@@ -10,41 +10,29 @@ import SwiftUI
 struct VerticalCarousel: Layout {
     var selectedIndex: Int
     
-    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout [CGSize]) -> CGSize {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
         proposal.replacingUnspecifiedDimensions()
     }
     
-    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout [CGSize]) {
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
         let selectedIndex = Self.clamp(value: selectedIndex, to: 1...subviews.count)
         var y = 0.0
         let proposal: ProposedViewSize = .init(width: bounds.width, height: bounds.height)
         
         // get initial y placement
-        for index in 0..<selectedIndex - 1 {
-            let height = cache[index].height
-            let nextHeight = cache[safe: index + 1]?.height ?? .zero
-            y -= (height / 2 + nextHeight / 2)
+        for (index, upperSubview) in subviews.prefix(upTo: selectedIndex - 1).enumerated() {
+            let size = upperSubview.sizeThatFits(proposal)
+            let nextHeight = subviews[safe: index + 1]?.sizeThatFits(proposal).height ?? .zero
+            y -= (size.height / 2 + nextHeight / 2)
         }
         
         // place each subview in vertical order
         for (index, subview) in subviews.enumerated() {
-            let size = cache[index]
+            let size = subview.sizeThatFits(proposal)
             subview.place(at: .init(x: bounds.minX, y: y + bounds.midY), anchor: .leading, proposal: proposal)
             
-            let nextHeight = cache[safe: index + 1]?.height ?? .zero
+            let nextHeight = subviews[safe: index + 1]?.sizeThatFits(proposal).height ?? .zero
             y += (size.height / 2 + nextHeight / 2)
-        }
-    }
-    
-    func makeCache(subviews: Subviews) -> [CGSize] {
-        subviews.map { subview in
-            subview.sizeThatFits(.infinity)
-        }
-    }
-    
-    func updateCache(_ cache: inout [CGSize], subviews: Subviews) {
-        cache = subviews.map { subview in
-            subview.sizeThatFits(.infinity)
         }
     }
     
